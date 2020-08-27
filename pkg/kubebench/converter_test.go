5848a1d15d541942a5c22b1d9bf5bd2d84258f9e
Daniel Pacak <pacak.daniel@gmail.com>
2020-08-27 13:08:41 +0200

    refactor: Simplify JSON decoding
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/kubebench/converter_test.go
+++ pkg/kubebench/converter_test.go
@@ -3,8 +3,6 @@ package kubebench
 import (
 	"encoding/json"
 	"errors"
-	"io"
-	"log"
 	"os"
 	"testing"
 
@@ -20,37 +18,49 @@ func TestConverter_Convert(t *testing.T) {
 		op   string // golden file
 		err  error  // expected error
 	}{
-		{"Valid single json object in array", "testdata/valid.json", "testdata/goldenSingle.json", nil},
-		{"invalid json object", "testdata/invalid.json", "testdata/goldenSingle.json", errors.New("json: cannot unmarshal object into Go value of type []v1alpha1.CISKubeBenchSection")},
-		{"Valid multiple json object in array", "testdata/multiObjects.json", "testdata/goldenMultiple.json", nil},
+		{
+			name: "Valid single json object in array",
+			in:   "testdata/valid.json",
+			op:   "testdata/goldenSingle.json",
+			err:  nil,
+		},
+		{
+			name: "invalid json object",
+			in:   "testdata/invalid.json",
+			err:  errors.New("json: cannot unmarshal object into Go value of type []v1alpha1.CISKubeBenchSection"),
+		},
+		{
+			name: "Valid multiple json object in array",
+			in:   "testdata/multiObjects.json",
+			op:   "testdata/goldenMultiple.json",
+			err:  nil,
+		},
 	}
 
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-			gFile, err := os.Open(tc.op)
-			require.NoError(t, err)
-			dec := json.NewDecoder(gFile)
-			var kbop starboard.CISKubeBenchOutput
-			for {
-				if err = dec.Decode(&kbop); err == io.EOF {
-					break
-				} else if err != nil {
-					log.Fatalln("Error while decoding golden file", err)
-				}
-			}
-
 			inFile, err := os.Open(tc.in)
 			require.NoError(t, err)
 			defer func() {
 				_ = inFile.Close()
-				_ = gFile.Close()
 			}()
 
 			var r starboard.CISKubeBenchOutput
 			r, err = DefaultConverter.Convert(inFile)
+
 			switch {
 			case tc.err == nil:
 				require.NoError(t, err)
+				gFile, err := os.Open(tc.op)
+				require.NoError(t, err)
+				dec := json.NewDecoder(gFile)
+				var kbop starboard.CISKubeBenchOutput
+				err = dec.Decode(&kbop)
+				require.NoError(t, err)
+				defer func() {
+					_ = gFile.Close()
+				}()
+
 				assert.Equal(t, kbop, r, "Converted report does not match expected report")
 			default:
 				assert.EqualError(t, err, tc.err.Error())
