f988dc38ae2472cbe85e5440198073e42cef8f67
Pete Travis <ptravis@groupon.com>
2020-11-10 12:58:16 -0600

    check environment variables for non-default configmap name and namespace

--- pkg/starboard/constants.go
+++ pkg/starboard/constants.go
@@ -1,14 +1,24 @@
 package starboard
 
-const (
-	// NamespaceName the name of the namespace in which Starboard stores its
-	// configuration and runs scan Jobs.
-	NamespaceName = "starboard"
-
-	// ServiceAccountName the name of the ServiceAccount used to run scan Jobs.
-	ServiceAccountName = "starboard"
-
-	// ConfigMapName the name of the ConfigMap that stored configuration of
-	// Starboard and the underlying scanners.
-	ConfigMapName = "starboard"
+import (
+  "os"
 )
+
+func getEnv(key, fallback string) string {
+    value, exists := os.LookupEnv(key)
+    if !exists {
+        value = fallback
+    }
+    return value
+}
+
+// NamespaceName the name of the namespace in which Starboard stores its
+// configuration and runs scan Jobs.
+var NamespaceName = getEnv("STARBOARD_CONFIG_NAMESPACE", "starboard")
+
+// ServiceAccountName the name of the ServiceAccount used to run scan Jobs.
+var	ServiceAccountName = getEnv("STARBOARD_SA_NAME", "starboard")
+
+// ConfigMapName the name of the ConfigMap that stored configuration of
+// Starboard and the underlying scanners.
+var ConfigMapName = getEnv("STARBOARD_CONFIGMAP_NAME", "starboard")
