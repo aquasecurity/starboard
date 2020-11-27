2082ab8e0e16dad7311ff7e866c5beda3ca8c6ca
Daniel Pacak <pacak.daniel@gmail.com>
2020-11-27 17:12:57 +0100

    fix: Compilation erros and remove duplicate code
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/cmd/kube_bench.go
+++ pkg/cmd/kube_bench.go
@@ -1,83 +1,18 @@
 package cmd
 
 import (
-	"context"
-	"fmt"
-	"sync"
-
-	"github.com/aquasecurity/starboard/pkg/starboard"
-
-	corev1 "k8s.io/api/core/v1"
-
-	starboardapi "github.com/aquasecurity/starboard/pkg/generated/clientset/versioned"
-	"github.com/aquasecurity/starboard/pkg/kubebench"
-	"github.com/aquasecurity/starboard/pkg/kubebench/crd"
 	"github.com/spf13/cobra"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/klog"
 )
 
+// Deprecated
+// Use NewScanKubeBenchReportsCmd instead.
 func NewKubeBenchCmd(cf *genericclioptions.ConfigFlags) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:        "kube-bench",
-		Deprecated: "'kube-bench' is deprecated, please use 'scan ciskubebenchreports'",
-		Short:      "Run the CIS Kubernetes Benchmark https://www.cisecurity.org/benchmark/kubernetes",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx := context.Background()
-			kubernetesConfig, err := cf.ToRESTConfig()
-			if err != nil {
-				return err
-			}
-			kubernetesClientset, err := kubernetes.NewForConfig(kubernetesConfig)
-			if err != nil {
-				return err
-			}
-			opts, err := getScannerOpts(cmd)
-			if err != nil {
-				return err
-			}
-			starboardClientset, err := starboardapi.NewForConfig(kubernetesConfig)
-			if err != nil {
-				return err
-			}
-			nodeList, err := kubernetesClientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
-			if err != nil {
-				return fmt.Errorf("listing nodes: %w", err)
-			}
-			config, err := starboard.NewConfigManager(kubernetesClientset, starboard.NamespaceName).Read(ctx)
-			if err != nil {
-				return err
-			}
-
-			scanner := kubebench.NewScanner(config, opts, kubernetesClientset)
-			writer := crd.NewReadWriter(starboard.NewScheme(), starboardClientset)
-
-			var wg sync.WaitGroup
-
-			for _, node := range nodeList.Items {
-				wg.Add(1)
-				go func(node corev1.Node) {
-					defer wg.Done()
-
-					report, err := scanner.Scan(ctx, node)
-
-					if err != nil {
-						klog.Errorf("Error while running kube-bench on node: %s: %v", node.Name, err)
-						return
-					}
-					err = writer.Write(ctx, report, &node)
-					if err != nil {
-						klog.Errorf("Error while writing kube-bench report for node: %s: %v", node.Name, err)
-						return
-					}
-				}(node)
-			}
-
-			wg.Wait()
-			return nil
-		},
+		Deprecated: "please use 'scan ciskubebenchreports' instead",
+		Short:      kubeBenchCmdShort,
+		RunE:       ScanKubeBenchReports(cf),
 	}
 
 	registerScannerOpts(cmd)
--- pkg/cmd/scan_kubehunter.go
+++ pkg/cmd/scan_kubehunter.go
@@ -11,41 +11,49 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+const (
+	kubeHunterCmdShort = "Hunt for security weaknesses in your Kubernetes cluster"
+)
+
 func NewScanKubeHunterReportsCmd(cf *genericclioptions.ConfigFlags) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "kubehunterreports",
-		Short: "Hunt for security weaknesses",
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			ctx := context.Background()
-			config, err := cf.ToRESTConfig()
-			if err != nil {
-				return
-			}
-			kubernetesClientset, err := kubernetes.NewForConfig(config)
-			if err != nil {
-				return
-			}
-			opts, err := getScannerOpts(cmd)
-			if err != nil {
-				return
-			}
-			report, err := kubehunter.NewScanner(opts, kubernetesClientset).Scan(ctx)
-			if err != nil {
-				return
-			}
-			starboardClientset, err := starboardapi.NewForConfig(config)
-			if err != nil {
-				return
-			}
-			err = crd.NewWriter(starboardClientset).Write(ctx, report, "cluster")
-			if err != nil {
-				return
-			}
-			return
-		},
+		Short: kubeHunterCmdShort,
+		RunE:  ScanKubeHunterReports(cf),
 	}
 
 	registerScannerOpts(cmd)
 
 	return cmd
+}
+
+func ScanKubeHunterReports(cf *genericclioptions.ConfigFlags) func(cmd *cobra.Command, args []string) (err error) {
+	return func(cmd *cobra.Command, args []string) (err error) {
+		ctx := context.Background()
+		config, err := cf.ToRESTConfig()
+		if err != nil {
+			return
+		}
+		kubernetesClientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			return
+		}
+		opts, err := getScannerOpts(cmd)
+		if err != nil {
+			return
+		}
+		report, err := kubehunter.NewScanner(opts, kubernetesClientset).Scan(ctx)
+		if err != nil {
+			return
+		}
+		starboardClientset, err := starboardapi.NewForConfig(config)
+		if err != nil {
+			return
+		}
+		err = crd.NewWriter(starboardClientset).Write(ctx, report, "cluster")
+		if err != nil {
+			return
+		}
+		return
+	}
 }
