c8deb20325b8874173dd03c8dad3460014c4396e
Daniel Pacak <pacak.daniel@gmail.com>
2021-01-11 21:11:35 +0100

    refactor: kubehunter package
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

---
+++ pkg/kubehunter/writer_test.go
@@ -0,0 +1,103 @@
+package kubehunter_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aquasecurity/starboard/pkg/apis/aquasecurity/v1alpha1"
+	"github.com/aquasecurity/starboard/pkg/generated/clientset/versioned/fake"
+	"github.com/aquasecurity/starboard/pkg/kube"
+	"github.com/aquasecurity/starboard/pkg/kubehunter"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestWriter_Write(t *testing.T) {
+
+	t.Run("Should create KubeHunterReport", func(t *testing.T) {
+		clientset := fake.NewSimpleClientset()
+
+		writer := kubehunter.NewWriter(clientset)
+		err := writer.Write(context.TODO(), v1alpha1.KubeHunterOutput{
+			Summary: v1alpha1.KubeHunterSummary{
+				HighCount: 7,
+			},
+		}, "my-cluster")
+		require.NoError(t, err)
+
+		list, err := clientset.AquasecurityV1alpha1().KubeHunterReports().List(context.TODO(), metav1.ListOptions{})
+		require.NoError(t, err)
+
+		reports := map[string]v1alpha1.KubeHunterReport{}
+		for _, item := range list.Items {
+			reports[item.Name] = item
+		}
+		assert.Equal(t, map[string]v1alpha1.KubeHunterReport{
+			"my-cluster": {
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-cluster",
+					Labels: map[string]string{
+						kube.LabelResourceKind: "Cluster",
+						kube.LabelResourceName: "my-cluster",
+					},
+				},
+				Report: v1alpha1.KubeHunterOutput{
+					Summary: v1alpha1.KubeHunterSummary{
+						HighCount: 7,
+					},
+				},
+			},
+		}, reports)
+	})
+
+	t.Run("Should update KubeHunterReport", func(t *testing.T) {
+		clientset := fake.NewSimpleClientset(&v1alpha1.KubeHunterReport{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "my-cluster",
+				Labels: map[string]string{
+					kube.LabelResourceKind: "Cluster",
+					kube.LabelResourceName: "my-cluster",
+				},
+			},
+			Report: v1alpha1.KubeHunterOutput{
+				Summary: v1alpha1.KubeHunterSummary{
+					HighCount: 1,
+				},
+			},
+		})
+
+		writer := kubehunter.NewWriter(clientset)
+		err := writer.Write(context.TODO(), v1alpha1.KubeHunterOutput{
+			Summary: v1alpha1.KubeHunterSummary{
+				HighCount: 3,
+			},
+		}, "my-cluster")
+		require.NoError(t, err)
+
+		list, err := clientset.AquasecurityV1alpha1().KubeHunterReports().List(context.TODO(), metav1.ListOptions{})
+		require.NoError(t, err)
+
+		reports := map[string]v1alpha1.KubeHunterReport{}
+		for _, item := range list.Items {
+			reports[item.Name] = item
+		}
+		assert.Equal(t, map[string]v1alpha1.KubeHunterReport{
+			"my-cluster": {
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-cluster",
+					Labels: map[string]string{
+						kube.LabelResourceKind: "Cluster",
+						kube.LabelResourceName: "my-cluster",
+					},
+				},
+				Report: v1alpha1.KubeHunterOutput{
+					Summary: v1alpha1.KubeHunterSummary{
+						HighCount: 3,
+					},
+				},
+			},
+		}, reports)
+	})
+
+}
