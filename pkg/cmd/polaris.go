56010615fa6c28c3807d5fd47665f5b912d39fd0
Daniel Pacak <pacak.daniel@gmail.com>
2021-03-05 00:15:47 +0100

    refactor: Use factory to instantiate ConfigAuditReport plugins
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/cmd/polaris.go
+++ pkg/cmd/polaris.go
@@ -1,19 +1,20 @@
 package cmd
 
 import (
+	"github.com/aquasecurity/starboard/pkg/starboard"
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
 // Deprecated
 // Use NewScanConfigAuditReportsCmd instead.
-func NewPolarisCmd(cf *genericclioptions.ConfigFlags) *cobra.Command {
+func NewPolarisCmd(buildInfo starboard.BuildInfo, cf *genericclioptions.ConfigFlags) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:        "polaris",
 		Deprecated: "please use 'scan configauditreports' instead",
 		Short:      configAuditCmdShort,
 		Args:       cobra.MaximumNArgs(1),
-		RunE:       ScanConfigAuditReports(cf),
+		RunE:       ScanConfigAuditReports(buildInfo, cf),
 	}
 
 	registerScannerOpts(cmd)
