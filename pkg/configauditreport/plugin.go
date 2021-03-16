a6f41e453e05b9e0d955f305ca4901b55173b96f
Daniel Pacak <pacak.daniel@gmail.com>
2021-03-16 16:33:41 +0100

    refactor: Remove redundant args passed to configauditreport.Plugin
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/configauditreport/plugin.go
+++ pkg/configauditreport/plugin.go
@@ -4,9 +4,7 @@ import (
 	"io"
 
 	"github.com/aquasecurity/starboard/pkg/apis/aquasecurity/v1alpha1"
-	"github.com/aquasecurity/starboard/pkg/kube"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -15,9 +13,18 @@ import (
 // first step to separate generic workloads discovery code and Polaris
 // implementation details.
 type Plugin interface {
-	GetScanJobSpec(workload kube.Object, obj client.Object, gvk schema.GroupVersionKind) (corev1.PodSpec, []*corev1.Secret, error)
 
+	// GetScanJobSpec describes the pod that will be created by Starboard when
+	// it schedules a Kubernetes job to scan the specified workload client.Object.
+	// The plugin might return zero to many v1.Secret objects which will be
+	// created by Starboard and associated with the scan job.
+	GetScanJobSpec(obj client.Object) (corev1.PodSpec, []*corev1.Secret, error)
+
+	// ParseConfigAuditReportData is a callback to parse and convert logs of
+	// the container in a pod controlled by the scan job to v1alpha1.ConfigAuditResult.
+	ParseConfigAuditReportData(logsReader io.ReadCloser) (v1alpha1.ConfigAuditResult, error)
+
+	// GetContainerName returns the name of the container in a pod created by a scan job
+	// to read logs from.
 	GetContainerName() string
-
-	ParseConfigAuditResult(logsReader io.ReadCloser) (v1alpha1.ConfigAuditResult, error)
 }
