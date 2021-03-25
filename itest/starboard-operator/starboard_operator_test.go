98d5ad2f3d14e8d2422b504f9c2620970d536580
mohammad <49061426+xyoxo@users.noreply.github.com>
2021-03-25 16:49:52 +0600

    test(operator): Assert that Deployment is rescanned after a rolling update (#457)
    Resolves: #454

--- itest/starboard-operator/starboard_operator_test.go
+++ itest/starboard-operator/starboard_operator_test.go
@@ -13,23 +13,23 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
 
 const (
-	assertionTimeout = 3 * time.Minute
+	assertionTimeout   = 3 * time.Minute
+	namespaceName      = corev1.NamespaceDefault
+	baseDeploymentName = "wordpress"
 )
 
 var _ = Describe("Starboard Operator", func() {
-
-	const (
-		namespaceName  = corev1.NamespaceDefault
-		deploymentName = "wordpress"
-	)
 
 	Describe("When unmanaged Pod is created", func() {
 
@@ -70,38 +70,12 @@ var _ = Describe("Starboard Operator", func() {
 	Describe("When Deployment is created", func() {
 
 		ctx := context.Background()
+		deploy := GetWordPressDeployment()
+		deploymentName := deploy.Name
 
 		BeforeEach(func() {
 			_, err := kubeClientset.AppsV1().Deployments(namespaceName).
-				Create(ctx, &appsv1.Deployment{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      deploymentName,
-						Namespace: namespaceName,
-					},
-					Spec: appsv1.DeploymentSpec{
-						Replicas: pointer.Int32Ptr(1),
-						Selector: &metav1.LabelSelector{
-							MatchLabels: map[string]string{
-								"app": "wordpress",
-							},
-						},
-						Template: corev1.PodTemplateSpec{
-							ObjectMeta: metav1.ObjectMeta{
-								Labels: labels.Set{
-									"app": "wordpress",
-								},
-							},
-							Spec: corev1.PodSpec{
-								Containers: []corev1.Container{
-									{
-										Name:  "wordpress",
-										Image: "wordpress:4.9",
-									},
-								},
-							},
-						},
-					},
-				}, metav1.CreateOptions{})
+				Create(ctx, deploy, metav1.CreateOptions{})
 			Expect(err).ToNot(HaveOccurred())
 
 			Eventually(HasActiveReplicaSet(namespaceName, deploymentName), assertionTimeout).Should(BeTrue())
@@ -123,7 +97,56 @@ var _ = Describe("Starboard Operator", func() {
 		})
 	})
 
-	// TODO Add a scenario for rolling update, i.e. create Deployment and then update its container image.
+	Describe("When Deployment is rolling updated", func() {
+
+		ctx := context.Background()
+		deploy := GetWordPressDeployment()
+		deploymentName := deploy.Name
+
+		BeforeEach(func() {
+			By("Creating Deployment wordpress")
+			_, err := kubeClientset.AppsV1().Deployments(namespaceName).
+				Create(ctx, deploy, metav1.CreateOptions{})
+			Expect(err).ToNot(HaveOccurred())
+
+			Eventually(HasActiveReplicaSet(namespaceName, deploymentName), assertionTimeout).Should(BeTrue())
+		})
+
+		It("Should create VulnerabilityReport and ConfigAuditReport for new Replicaset", func() {
+			By("Getting current active replicaset")
+			rs, err := GetActiveReplicaSetForDeployment(namespaceName, deploymentName)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(rs).ToNot(BeNil())
+
+			By("Waiting for Config Audit Report")
+			Eventually(HasConfigAuditReportOwnedBy(rs), assertionTimeout).Should(BeTrue())
+			By("Waiting for Vulnerability Report")
+			Eventually(HasVulnerabilityReportOwnedBy(rs), assertionTimeout).Should(BeTrue())
+
+			By("Updating deployment image to wordpress:5")
+			err = UpdateDeploymentImage(namespaceName, deploymentName)
+			Expect(err).ToNot(HaveOccurred())
+
+			Eventually(HasActiveReplicaSet(namespaceName, deploymentName), assertionTimeout).Should(BeTrue())
+
+			By("Getting new active replicaset")
+			rs, err = GetActiveReplicaSetForDeployment(namespaceName, deploymentName)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(rs).ToNot(BeNil())
+
+			By("Waiting for new Config Audit Report")
+			Eventually(HasConfigAuditReportOwnedBy(rs), assertionTimeout).Should(BeTrue())
+
+			By("Waiting for new Vulnerability Report")
+			Eventually(HasVulnerabilityReportOwnedBy(rs), assertionTimeout).Should(BeTrue())
+		})
+
+		AfterEach(func() {
+			err := kubeClientset.AppsV1().Deployments(namespaceName).
+				Delete(ctx, deploymentName, metav1.DeleteOptions{})
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
 
 	Describe("When CronJob is created", func() {
 		var cronJob *batchv1beta1.CronJob
@@ -271,5 +294,55 @@ func HasCISKubeBenchReportOwnedBy(node corev1.Node) func() (bool, error) {
 			return false, err
 		}
 		return report != nil, nil
+	}
+}
+
+func UpdateDeploymentImage(namespace, name string) error {
+	return wait.PollImmediate(5*time.Second, 2*time.Minute, func() (done bool, err error) {
+		deployment, err := kubeClientset.AppsV1().Deployments(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+
+		dcDeploy := deployment.DeepCopy()
+		dcDeploy.Spec.Template.Spec.Containers[0].Image = "wordpress:5"
+		_, err = kubeClientset.AppsV1().Deployments(namespace).Update(context.TODO(), dcDeploy, metav1.UpdateOptions{})
+		if err != nil && errors.IsConflict(err) {
+			return false, nil
+		}
+
+		return err == nil, err
+	})
+}
+
+func GetWordPressDeployment() *appsv1.Deployment {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      baseDeploymentName + "-" + rand.String(5),
+			Namespace: namespaceName,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: pointer.Int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "wordpress",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels.Set{
+						"app": "wordpress",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "wordpress",
+							Image: "wordpress:4.9",
+						},
+					},
+				},
+			},
+		},
 	}
 }
