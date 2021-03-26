2060f7bc7cb5613ae9b211d849869282db123fb9
Daniel Pacak <pacak.daniel@gmail.com>
2021-03-26 23:38:02 +0100

    refactor: Use custom Gomega matcher to assert VulnerabilityReports (#461)
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

---
+++ itest/matcher/matcher.go
@@ -0,0 +1,163 @@
+package matcher
+
+import (
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+
+	"fmt"
+
+	"github.com/aquasecurity/starboard/pkg/apis/aquasecurity/v1alpha1"
+	"github.com/aquasecurity/starboard/pkg/kube"
+	"github.com/aquasecurity/starboard/pkg/starboard"
+	"github.com/onsi/gomega/types"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+)
+
+var (
+	trivyScanner = v1alpha1.Scanner{
+		Name:    "Trivy",
+		Vendor:  "Aqua Security",
+		Version: "0.16.0",
+	}
+	polarisScanner = v1alpha1.Scanner{
+		Name:    "Polaris",
+		Vendor:  "Fairwinds Ops",
+		Version: "3.2",
+	}
+)
+
+// IsVulnerabilityReportForContainerOwnedBy succeeds if a v1alpha1.VulnerabilityReport has a valid structure,
+// corresponds to the given container and is owned by the specified client.Object.
+//
+// Note: This matcher is not suitable for unit tests because it does not perform a strict validation
+// of the actual v1alpha1.VulnerabilityReport.
+func IsVulnerabilityReportForContainerOwnedBy(containerName string, owner client.Object) types.GomegaMatcher {
+	return &vulnerabilityReportMatcher{
+		containerName: containerName,
+		owner:         owner,
+	}
+}
+
+type vulnerabilityReportMatcher struct {
+	owner                 client.Object
+	containerName         string
+	failureMessage        string
+	negatedFailureMessage string
+}
+
+func (m *vulnerabilityReportMatcher) Match(actual interface{}) (bool, error) {
+	_, ok := actual.(v1alpha1.VulnerabilityReport)
+	if !ok {
+		return false, fmt.Errorf("%T expects a %T", vulnerabilityReportMatcher{}, v1alpha1.VulnerabilityReport{})
+	}
+	gvk, err := apiutil.GVKForObject(m.owner, starboard.NewScheme())
+	if err != nil {
+		return false, err
+	}
+
+	matcher := MatchFields(IgnoreExtras, Fields{
+		"ObjectMeta": MatchFields(IgnoreExtras, Fields{
+			"Labels": MatchAllKeys(Keys{
+				kube.LabelContainerName:     Equal(m.containerName),
+				kube.LabelResourceKind:      Equal(gvk.Kind),
+				kube.LabelResourceName:      Equal(m.owner.GetName()),
+				kube.LabelResourceNamespace: Equal(m.owner.GetNamespace()),
+			}),
+			"OwnerReferences": ConsistOf(metav1.OwnerReference{
+				APIVersion: gvk.GroupVersion().Identifier(),
+				Kind:       gvk.Kind,
+				Name:       m.owner.GetName(),
+				UID:        m.owner.GetUID(),
+			}),
+		}),
+		"Report": MatchFields(IgnoreExtras, Fields{
+			"Scanner":         Equal(trivyScanner),
+			"Vulnerabilities": Not(BeNil()),
+		}),
+	})
+
+	success, err := matcher.Match(actual)
+	if err != nil {
+		return false, err
+	}
+	m.failureMessage = matcher.FailureMessage(actual)
+	m.negatedFailureMessage = matcher.NegatedFailureMessage(actual)
+	return success, nil
+}
+
+func (m *vulnerabilityReportMatcher) FailureMessage(_ interface{}) string {
+	// TODO Add more descriptive message rather than rely on composed matchers' defaults
+	return m.failureMessage
+}
+
+func (m *vulnerabilityReportMatcher) NegatedFailureMessage(_ interface{}) string {
+	return m.negatedFailureMessage
+}
+
+// IsConfigAuditReportOwnedBy succeeds if a v1alpha1.ConfigAuditReport has a valid structure,
+// and is owned by the specified client.Object.
+//
+// Note: This matcher is not suitable for unit tests because it does not perform a strict validation
+// of the actual v1alpha1.ConfigAuditReport.
+func IsConfigAuditReportOwnedBy(owner client.Object) types.GomegaMatcher {
+	return &configAuditReportMatcher{
+		owner: owner,
+	}
+}
+
+type configAuditReportMatcher struct {
+	owner                 client.Object
+	failureMessage        string
+	negatedFailureMessage string
+}
+
+func (m *configAuditReportMatcher) Match(actual interface{}) (bool, error) {
+	_, ok := actual.(v1alpha1.ConfigAuditReport)
+	if !ok {
+		return false, fmt.Errorf("%T expects a %T", configAuditReportMatcher{}, v1alpha1.ConfigAuditReport{})
+	}
+	gvk, err := apiutil.GVKForObject(m.owner, starboard.NewScheme())
+	if err != nil {
+		return false, err
+	}
+
+	matcher := MatchFields(IgnoreExtras, Fields{
+		"ObjectMeta": MatchFields(IgnoreExtras, Fields{
+			"Labels": MatchAllKeys(Keys{
+				kube.LabelResourceKind:      Equal(gvk.Kind),
+				kube.LabelResourceName:      Equal(m.owner.GetName()),
+				kube.LabelResourceNamespace: Equal(m.owner.GetNamespace()),
+			}),
+			"OwnerReferences": ConsistOf(metav1.OwnerReference{
+				APIVersion:         gvk.GroupVersion().Identifier(),
+				Kind:               gvk.Kind,
+				Name:               m.owner.GetName(),
+				UID:                m.owner.GetUID(),
+				Controller:         pointer.BoolPtr(true),
+				BlockOwnerDeletion: pointer.BoolPtr(true),
+			}),
+		}),
+		"Report": MatchFields(IgnoreExtras, Fields{
+			"Scanner": Equal(polarisScanner),
+		}),
+	})
+	success, err := matcher.Match(actual)
+	if err != nil {
+		return false, err
+	}
+	m.failureMessage = matcher.FailureMessage(actual)
+	m.negatedFailureMessage = matcher.NegatedFailureMessage(actual)
+	return success, nil
+}
+
+func (m *configAuditReportMatcher) FailureMessage(_ interface{}) string {
+	// TODO Add more descriptive message rather than rely on composed matchers' defaults
+	return m.failureMessage
+}
+
+func (m *configAuditReportMatcher) NegatedFailureMessage(_ interface{}) string {
+	return m.negatedFailureMessage
+}
---
+++ itest/matcher/matcher_test.go
@@ -0,0 +1,117 @@
+package matcher_test
+
+import (
+	. "github.com/onsi/gomega"
+
+	"testing"
+
+	"github.com/aquasecurity/starboard/itest/matcher"
+	"github.com/aquasecurity/starboard/pkg/apis/aquasecurity/v1alpha1"
+	"github.com/aquasecurity/starboard/pkg/kube"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+)
+
+func TestVulnerabilityReportMatcher(t *testing.T) {
+
+	t.Run("Should return error when actual is not VulnerabilityReport", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		instance := matcher.IsVulnerabilityReportForContainerOwnedBy("nginx", &corev1.Pod{})
+		_, err := instance.Match("I AM INVALID ACTUAL")
+		g.Expect(err).To(MatchError("matcher.vulnerabilityReportMatcher expects a v1alpha1.VulnerabilityReport"))
+	})
+
+	t.Run("Should return true when VulnerabilityReport matches", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		instance := matcher.IsVulnerabilityReportForContainerOwnedBy("nginx-container", &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "nginx-pod",
+				Namespace: "default",
+				UID:       "56d53a84-c81b-4620-81a1-e226c35d3983",
+			},
+		})
+		success, err := instance.Match(v1alpha1.VulnerabilityReport{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "pod-nginx-pod-nginx-container",
+				Namespace: "default",
+				Labels: map[string]string{
+					kube.LabelContainerName:     "nginx-container",
+					kube.LabelResourceKind:      "Pod",
+					kube.LabelResourceName:      "nginx-pod",
+					kube.LabelResourceNamespace: "default",
+				},
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						Name:       "nginx-pod",
+						UID:        "56d53a84-c81b-4620-81a1-e226c35d3983",
+					},
+				},
+			},
+			Report: v1alpha1.VulnerabilityScanResult{
+				Scanner: v1alpha1.Scanner{
+					Name:    "Trivy",
+					Vendor:  "Aqua Security",
+					Version: "0.16.0",
+				},
+				Vulnerabilities: []v1alpha1.Vulnerability{},
+			},
+		})
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(success).To(BeTrue())
+	})
+}
+
+func TestConfigAuditReportMatcher(t *testing.T) {
+
+	t.Run("Should return error when actual is not ConfigAuditReport", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		instance := matcher.IsConfigAuditReportOwnedBy(&appsv1.ReplicaSet{})
+		_, err := instance.Match("I AM INVALID ACTUAL")
+		g.Expect(err).To(MatchError("matcher.configAuditReportMatcher expects a v1alpha1.ConfigAuditReport"))
+	})
+
+	t.Run("Should return true when ConfigAuditReport matches", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		instance := matcher.IsConfigAuditReportOwnedBy(&appsv1.ReplicaSet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "nginx-6d4cf56db6",
+				Namespace: "default",
+				UID:       "494b2727-5d52-4057-9a9b-8b508c753fea",
+			},
+		})
+		success, err := instance.Match(v1alpha1.ConfigAuditReport{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "replicaset-nginx-6d4cf56db6",
+				Namespace: "default",
+				Labels: map[string]string{
+					kube.LabelResourceKind:      "ReplicaSet",
+					kube.LabelResourceName:      "nginx-6d4cf56db6",
+					kube.LabelResourceNamespace: "default",
+				},
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         "apps/v1",
+						Kind:               "ReplicaSet",
+						Name:               "nginx-6d4cf56db6",
+						UID:                "494b2727-5d52-4057-9a9b-8b508c753fea",
+						Controller:         pointer.BoolPtr(true),
+						BlockOwnerDeletion: pointer.BoolPtr(true),
+					},
+				},
+			},
+			Report: v1alpha1.ConfigAuditResult{
+				Scanner: v1alpha1.Scanner{
+					Name:    "Polaris",
+					Vendor:  "Fairwinds Ops",
+					Version: "3.2",
+				},
+			},
+		})
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(success).To(BeTrue())
+	})
+}
