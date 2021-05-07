66cf6a25fff1e3938abd74174ffdf5c2be404958
Daniel Pacak <pacak.daniel@gmail.com>
2021-05-07 15:10:02 +0200

    refactor: Use global shared behavior to reuse test specs (#555)
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- itest/helper/helper.go
+++ itest/helper/helper.go
@@ -1,17 +1,26 @@
 package helper
 
 import (
+	"context"
+	"fmt"
 	"time"
 
 	"github.com/aquasecurity/starboard/pkg/apis/aquasecurity/v1alpha1"
 	"github.com/aquasecurity/starboard/pkg/kube"
+	"github.com/aquasecurity/starboard/pkg/kubebench"
 	"github.com/aquasecurity/starboard/pkg/starboard"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
 
 type PodBuilder struct {
@@ -206,4 +215,149 @@ func (b *VulnerabilityReportBuilder) Build() *v1alpha1.VulnerabilityReport {
 			},
 		},
 	}
+}
+
+// Helper is a mix of asserts and helpers, but we can fix that later.
+type Helper struct {
+	scheme                *runtime.Scheme
+	kubeClient            client.Client
+	kubeBenchReportReader kubebench.Reader
+}
+
+func NewHelper(scheme *runtime.Scheme, client client.Client) *Helper {
+	return &Helper{
+		scheme:                scheme,
+		kubeClient:            client,
+		kubeBenchReportReader: kubebench.NewReadWriter(client),
+	}
+}
+
+func (h *Helper) HasActiveReplicaSet(namespace, name string) func() (bool, error) {
+	return func() (bool, error) {
+		rs, err := h.GetActiveReplicaSetForDeployment(namespace, name)
+		if err != nil {
+			return false, err
+		}
+		return rs != nil, nil
+	}
+}
+
+func (h *Helper) HasVulnerabilityReportOwnedBy(obj client.Object) func() (bool, error) {
+	return func() (bool, error) {
+		gvk, err := apiutil.GVKForObject(obj, h.scheme)
+		if err != nil {
+			return false, err
+		}
+		var reportList v1alpha1.VulnerabilityReportList
+		err = h.kubeClient.List(context.Background(), &reportList, client.MatchingLabels{
+			starboard.LabelResourceKind:      gvk.Kind,
+			starboard.LabelResourceName:      obj.GetName(),
+			starboard.LabelResourceNamespace: obj.GetNamespace(),
+		})
+		if err != nil {
+			return false, err
+		}
+		return len(reportList.Items) == 1, nil
+	}
+}
+
+func (h *Helper) HasConfigAuditReportOwnedBy(obj client.Object) func() (bool, error) {
+	return func() (bool, error) {
+		gvk, err := apiutil.GVKForObject(obj, h.scheme)
+		if err != nil {
+			return false, err
+		}
+		var reportsList v1alpha1.ConfigAuditReportList
+		err = h.kubeClient.List(context.Background(), &reportsList, client.MatchingLabels{
+			starboard.LabelResourceKind:      gvk.Kind,
+			starboard.LabelResourceName:      obj.GetName(),
+			starboard.LabelResourceNamespace: obj.GetNamespace(),
+		})
+		if err != nil {
+			return false, err
+		}
+
+		return len(reportsList.Items) == 1 && reportsList.Items[0].DeletionTimestamp == nil, nil
+	}
+}
+
+func (h *Helper) DeleteConfigAuditReportOwnedBy(obj client.Object) error {
+	gvk, err := apiutil.GVKForObject(obj, h.scheme)
+	if err != nil {
+		return err
+	}
+	var reportsList v1alpha1.ConfigAuditReportList
+	err = h.kubeClient.List(context.Background(), &reportsList, client.MatchingLabels{
+		starboard.LabelResourceKind:      gvk.Kind,
+		starboard.LabelResourceName:      obj.GetName(),
+		starboard.LabelResourceNamespace: obj.GetNamespace(),
+	})
+	if err != nil {
+		return err
+	}
+
+	return h.kubeClient.Delete(context.Background(), &reportsList.Items[0])
+}
+
+func (h *Helper) GetActiveReplicaSetForDeployment(namespace, name string) (*appsv1.ReplicaSet, error) {
+	var deployment appsv1.Deployment
+	var replicaSetList appsv1.ReplicaSetList
+
+	err := h.kubeClient.Get(context.TODO(), types.NamespacedName{
+		Name: name, Namespace: namespace,
+	}, &deployment)
+	if err != nil {
+		return nil, err
+	}
+
+	deploymentSelector, err := metav1.LabelSelectorAsMap(deployment.Spec.Selector)
+	if err != nil {
+		return nil, fmt.Errorf("mapping label selector: %w", err)
+	}
+	selector := labels.Set(deploymentSelector)
+
+	err = h.kubeClient.List(context.TODO(), &replicaSetList, client.MatchingLabels(selector))
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, replicaSet := range replicaSetList.Items {
+		if deployment.Annotations["deployment.kubernetes.io/revision"] !=
+			replicaSet.Annotations["deployment.kubernetes.io/revision"] {
+			continue
+		}
+		return replicaSet.DeepCopy(), nil
+	}
+	return nil, nil
+}
+
+func (h *Helper) HasCISKubeBenchReportOwnedBy(node corev1.Node) func() (bool, error) {
+	return func() (bool, error) {
+		report, err := h.kubeBenchReportReader.FindByOwner(context.Background(), kube.Object{Kind: kube.KindNode, Name: node.Name})
+		if err != nil {
+			return false, err
+		}
+		return report != nil, nil
+	}
+}
+
+func (h *Helper) UpdateDeploymentImage(namespace, name string) error {
+	// TODO Check kubectl set image implementation
+	return wait.PollImmediate(5*time.Second, 2*time.Minute, func() (bool, error) {
+		var deployment appsv1.Deployment
+		err := h.kubeClient.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: namespace}, &deployment)
+		if err != nil {
+			return false, err
+		}
+
+		dcDeploy := deployment.DeepCopy()
+		dcDeploy.Spec.Template.Spec.Containers[0].Image = "wordpress:5"
+		err = h.kubeClient.Update(context.TODO(), dcDeploy)
+		if err != nil && errors.IsConflict(err) {
+			return false, nil
+		}
+
+		return err == nil, err
+	})
 }
---
+++ itest/starboard-operator/behavior.go
@@ -0,0 +1,418 @@
+package starboard_operator
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"context"
+	"time"
+
+	"github.com/aquasecurity/starboard/itest/helper"
+	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	batchv1beta1 "k8s.io/api/batch/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// SharedBehaviorInputs represents required inputs to shared behavior containers.
+type SharedBehaviorInputs struct {
+	AssertTimeout         time.Duration
+	PrimaryNamespace      string
+	PrimaryWorkloadPrefix string
+	client.Client
+	*helper.Helper
+}
+
+// VulnerabilityScannerBehavior returns the container of specs that describe behavior
+// of a vulnerability scanner with the given inputs.
+func VulnerabilityScannerBehavior(inputs *SharedBehaviorInputs) func() {
+	return func() {
+
+		Context("When unmanaged Pod is created", func() {
+
+			var ctx context.Context
+			var pod *corev1.Pod
+
+			BeforeEach(func() {
+				ctx = context.Background()
+				pod = helper.NewPod().
+					WithRandomName("unmanaged-nginx").
+					WithNamespace(inputs.PrimaryNamespace).
+					WithContainer("nginx", "nginx:1.16").
+					Build()
+
+				err := inputs.Create(ctx, pod)
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("Should create VulnerabilityReport", func() {
+				Eventually(inputs.HasVulnerabilityReportOwnedBy(pod), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, pod)
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+		})
+
+		Context("When Deployment is created", func() {
+
+			var ctx context.Context
+			var deploy *appsv1.Deployment
+
+			BeforeEach(func() {
+				ctx = context.Background()
+				deploy = helper.NewDeployment().
+					WithRandomName(inputs.PrimaryWorkloadPrefix).
+					WithNamespace(inputs.PrimaryNamespace).
+					WithContainer("wordpress", "wordpress:4.9").
+					Build()
+
+				err := inputs.Create(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(inputs.HasActiveReplicaSet(inputs.PrimaryNamespace, deploy.Name), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			It("Should create VulnerabilityReport", func() {
+				rs, err := inputs.GetActiveReplicaSetForDeployment(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rs).ToNot(BeNil())
+
+				Eventually(inputs.HasVulnerabilityReportOwnedBy(rs), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("When Deployment is rolling updated", func() {
+
+			var ctx context.Context
+			var deploy *appsv1.Deployment
+
+			BeforeEach(func() {
+				By("Creating Deployment wordpress")
+				ctx = context.Background()
+				deploy = helper.NewDeployment().
+					WithRandomName(inputs.PrimaryWorkloadPrefix).
+					WithNamespace(inputs.PrimaryNamespace).
+					WithContainer("wordpress", "wordpress:4.9").
+					Build()
+
+				err := inputs.Create(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(inputs.HasActiveReplicaSet(inputs.PrimaryNamespace, deploy.Name), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			It("Should create VulnerabilityReport for new ReplicaSet", func() {
+				By("Getting current active ReplicaSet")
+				rs, err := inputs.GetActiveReplicaSetForDeployment(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rs).ToNot(BeNil())
+
+				By("Waiting for VulnerabilityReport")
+				Eventually(inputs.HasVulnerabilityReportOwnedBy(rs), inputs.AssertTimeout).Should(BeTrue())
+
+				By("Updating deployment image to wordpress:5")
+				err = inputs.UpdateDeploymentImage(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+
+				Eventually(inputs.HasActiveReplicaSet(inputs.PrimaryNamespace, deploy.Name), inputs.AssertTimeout).Should(BeTrue())
+
+				By("Getting new active replicaset")
+				rs, err = inputs.GetActiveReplicaSetForDeployment(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rs).ToNot(BeNil())
+
+				By("Waiting for new VulnerabilityReport")
+				Eventually(inputs.HasVulnerabilityReportOwnedBy(rs), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("When CronJob is created", func() {
+
+			var ctx context.Context
+			var cronJob *batchv1beta1.CronJob
+
+			BeforeEach(func() {
+				ctx = context.Background()
+				cronJob = &batchv1beta1.CronJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: inputs.PrimaryNamespace,
+						Name:      "hello-" + rand.String(5),
+					},
+					Spec: batchv1beta1.CronJobSpec{
+						Schedule: "*/1 * * * *",
+						JobTemplate: batchv1beta1.JobTemplateSpec{
+							Spec: batchv1.JobSpec{
+								Template: corev1.PodTemplateSpec{
+									Spec: corev1.PodSpec{
+										RestartPolicy: corev1.RestartPolicyOnFailure,
+										Containers: []corev1.Container{
+											{
+												Name:  "hello",
+												Image: "busybox",
+												Command: []string{
+													"/bin/sh",
+													"-c",
+													"date; echo Hello from the Kubernetes cluster",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+				err := inputs.Create(ctx, cronJob)
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("Should create VulnerabilityReport", func() {
+				Eventually(inputs.HasVulnerabilityReportOwnedBy(cronJob), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, cronJob)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		// TODO Add scenario to test that VulnerabilityReport is recreated
+
+		// TODO Add scenario for workload with multiple containers
+
+		// TODO Add scenario for ReplicaSet
+
+		// TODO Add scenario for StatefulSet
+
+		// TODO Add scenario for DaemonSet
+	}
+}
+
+// ConfigurationCheckerBehavior returns the container of specs that describe behavior
+// of a configuration checker with the given inputs.
+func ConfigurationCheckerBehavior(inputs *SharedBehaviorInputs) func() {
+	return func() {
+
+		Context("When unmanaged Pod is created", func() {
+
+			var ctx context.Context
+			var pod *corev1.Pod
+
+			BeforeEach(func() {
+				ctx = context.Background()
+				pod = helper.NewPod().
+					WithRandomName("unmanaged-nginx").
+					WithNamespace(inputs.PrimaryNamespace).
+					WithContainer("nginx", "nginx:1.16").
+					Build()
+
+				err := inputs.Create(ctx, pod)
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("Should create ConfigAuditReport", func() {
+				Eventually(inputs.HasConfigAuditReportOwnedBy(pod), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, pod)
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+		})
+
+		Context("When Deployment is created", func() {
+
+			var ctx context.Context
+			var deploy *appsv1.Deployment
+
+			BeforeEach(func() {
+				ctx = context.Background()
+				deploy = helper.NewDeployment().
+					WithRandomName(inputs.PrimaryWorkloadPrefix).
+					WithNamespace(inputs.PrimaryNamespace).
+					WithContainer("wordpress", "wordpress:4.9").
+					Build()
+
+				err := inputs.Create(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(inputs.HasActiveReplicaSet(inputs.PrimaryNamespace, deploy.Name), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			It("Should create ConfigAuditReport", func() {
+				rs, err := inputs.GetActiveReplicaSetForDeployment(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rs).ToNot(BeNil())
+
+				Eventually(inputs.HasConfigAuditReportOwnedBy(rs), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("When Deployment is rolling updated", func() {
+
+			var ctx context.Context
+			var deploy *appsv1.Deployment
+
+			BeforeEach(func() {
+				By("Creating Deployment wordpress")
+				ctx = context.Background()
+				deploy = helper.NewDeployment().
+					WithRandomName(inputs.PrimaryWorkloadPrefix).
+					WithNamespace(inputs.PrimaryNamespace).
+					WithContainer("wordpress", "wordpress:4.9").
+					Build()
+
+				err := inputs.Create(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(inputs.HasActiveReplicaSet(inputs.PrimaryNamespace, deploy.Name), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			It("Should create ConfigAuditReport for new ReplicaSet", func() {
+				By("Getting current active ReplicaSet")
+				rs, err := inputs.GetActiveReplicaSetForDeployment(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rs).ToNot(BeNil())
+
+				By("Waiting for ConfigAuditReport")
+				Eventually(inputs.HasConfigAuditReportOwnedBy(rs), inputs.AssertTimeout).Should(BeTrue())
+
+				By("Updating deployment image to wordpress:5")
+				err = inputs.UpdateDeploymentImage(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+
+				Eventually(inputs.HasActiveReplicaSet(inputs.PrimaryNamespace, deploy.Name), inputs.AssertTimeout).Should(BeTrue())
+
+				By("Getting new active replicaset")
+				rs, err = inputs.GetActiveReplicaSetForDeployment(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rs).ToNot(BeNil())
+
+				By("Waiting for new Config Audit Report")
+				Eventually(inputs.HasConfigAuditReportOwnedBy(rs), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("When CronJob is created", func() {
+
+			var ctx context.Context
+			var cronJob *batchv1beta1.CronJob
+
+			BeforeEach(func() {
+				ctx = context.Background()
+				cronJob = &batchv1beta1.CronJob{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: inputs.PrimaryNamespace,
+						Name:      "hello-" + rand.String(5),
+					},
+					Spec: batchv1beta1.CronJobSpec{
+						Schedule: "*/1 * * * *",
+						JobTemplate: batchv1beta1.JobTemplateSpec{
+							Spec: batchv1.JobSpec{
+								Template: corev1.PodTemplateSpec{
+									Spec: corev1.PodSpec{
+										RestartPolicy: corev1.RestartPolicyOnFailure,
+										Containers: []corev1.Container{
+											{
+												Name:  "hello",
+												Image: "busybox",
+												Command: []string{
+													"/bin/sh",
+													"-c",
+													"date; echo Hello from the Kubernetes cluster",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+				err := inputs.Create(ctx, cronJob)
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("Should create ConfigAuditReport", func() {
+				Eventually(inputs.HasConfigAuditReportOwnedBy(cronJob), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, cronJob)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("When ConfigAuditReport is deleted", func() {
+
+			var ctx context.Context
+			var deploy *appsv1.Deployment
+
+			BeforeEach(func() {
+				By("Creating Deployment")
+				ctx = context.Background()
+				deploy = helper.NewDeployment().
+					WithRandomName(inputs.PrimaryWorkloadPrefix).
+					WithNamespace(inputs.PrimaryNamespace).
+					WithContainer("wordpress", "wordpress:4.9").
+					Build()
+
+				err := inputs.Create(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+				Eventually(inputs.HasActiveReplicaSet(inputs.PrimaryNamespace, deploy.Name), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			It("Should rescan Deployment when ConfigAuditReport is deleted", func() {
+				By("Getting active ReplicaSet")
+				rs, err := inputs.GetActiveReplicaSetForDeployment(inputs.PrimaryNamespace, deploy.Name)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rs).ToNot(BeNil())
+
+				By("Waiting for ConfigAuditReport")
+				Eventually(inputs.HasConfigAuditReportOwnedBy(rs), inputs.AssertTimeout).Should(BeTrue())
+				By("Deleting ConfigAuditReport")
+				err = inputs.DeleteConfigAuditReportOwnedBy(rs)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("Waiting for new ConfigAuditReport")
+				Eventually(inputs.HasConfigAuditReportOwnedBy(rs), inputs.AssertTimeout).Should(BeTrue())
+			})
+
+			AfterEach(func() {
+				err := inputs.Delete(ctx, deploy)
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		// TODO Add scenario for workload with multiple containers
+
+		// TODO Add scenario for ReplicaSet
+
+		// TODO Add scenario for StatefulSet
+
+		// TODO Add scenario for DaemonSet
+	}
+}
