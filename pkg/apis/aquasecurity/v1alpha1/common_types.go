540a389f9db6d117ec4c07bf10ec91c11e6455aa
Daniel Pacak <pacak.daniel@gmail.com>
2021-10-20 19:47:35 +0200

    feat: Explain VulnerabilityReport and ClusterVulnerabilityReport CRDs
    Resolves: #409
    
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/apis/aquasecurity/v1alpha1/common_types.go
+++ pkg/apis/aquasecurity/v1alpha1/common_types.go
@@ -2,7 +2,12 @@ package v1alpha1
 
 // Scanner is the spec for a scanner generating a security assessment report.
 type Scanner struct {
-	Name    string `json:"name"`
-	Vendor  string `json:"vendor"`
+	// Name the name of the scanner.
+	Name string `json:"name"`
+
+	// Vendor the name of the vendor providing the scanner.
+	Vendor string `json:"vendor"`
+
+	// Version the version of the scanner.
 	Version string `json:"version"`
 }
