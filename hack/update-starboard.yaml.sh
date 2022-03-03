9f726dae26d332a2dd0629332c28860461c3f79d
Daniel Pacak <pacak.daniel@gmail.com>
2022-03-03 18:52:18 +0100

    chore: add script to generate deploy/static/starboard.yaml (#996)
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

---
+++ hack/update-starboard.yaml.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
+CRD_ROOT=$SCRIPT_ROOT/deploy/crd
+STATIC_ROOT=$SCRIPT_ROOT/deploy/static
+
+cat $CRD_ROOT/vulnerabilityreports.crd.yaml \
+  $CRD_ROOT/configauditreports.crd.yaml \
+  $CRD_ROOT/clusterconfigauditreports.crd.yaml \
+  $CRD_ROOT/ciskubebenchreports.crd.yaml \
+  $STATIC_ROOT/01-starboard-operator.ns.yaml \
+  $STATIC_ROOT/02-starboard-operator.rbac.yaml \
+  $STATIC_ROOT/03-starboard-operator.config.yaml \
+  $STATIC_ROOT/04-starboard-operator.deployment.yaml > $STATIC_ROOT/starboard.yaml
