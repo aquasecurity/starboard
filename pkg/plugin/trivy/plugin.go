fcce9733b6cc9579826dc772bf9a9d575d0bf5b3
Maik <38863280+VF-mbrauer@users.noreply.github.com>
2022-05-25 16:05:56 +0200

    Added Region support for ECR-Registry handling. (#1191)

--- pkg/plugin/trivy/plugin.go
+++ pkg/plugin/trivy/plugin.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 	"strings"
 
 	"github.com/aquasecurity/starboard/pkg/apis/aquasecurity/v1alpha1"
@@ -24,6 +25,10 @@ import (
 const (
 	// Plugin the name of this plugin.
 	Plugin = "Trivy"
+)
+
+const (
+	AWSECR_Image_Regex = "^\\d+\\.dkr\\.ecr\\.(\\w+-\\w+-\\d+)\\.amazonaws\\.com\\/"
 )
 
 const (
@@ -586,6 +591,14 @@ func (p *plugin) getPodSpecForStandaloneMode(ctx starboard.PluginContext, config
 			env = append(env, corev1.EnvVar{
 				Name:  "TRIVY_IGNOREFILE",
 				Value: "/etc/trivy/.trivyignore",
+			})
+		}
+
+		region := CheckAwsEcrPrivateRegistry(c.Image)
+		if region != "" {
+			env = append(env, corev1.EnvVar{
+				Name:  "AWS_REGION",
+				Value: region,
 			})
 		}
 
@@ -1386,4 +1399,11 @@ func constructEnvVarSourceFromConfigMap(envName, configName, configKey string) (
 		},
 	}
 	return
+}
+
+func CheckAwsEcrPrivateRegistry(ImageUrl string) string {
+	if len(regexp.MustCompile(AWSECR_Image_Regex).FindAllStringSubmatch(ImageUrl, -1)) != 0 {
+		return regexp.MustCompile(AWSECR_Image_Regex).FindAllStringSubmatch(ImageUrl, -1)[0][1]
+	}
+	return ""
 }
